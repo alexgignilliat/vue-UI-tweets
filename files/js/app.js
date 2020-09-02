a28f2be5e3f8dd8c9b9e188c201120e7891fa2f8
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-09-02 19:31:11 -0400

    formatting code

--- files/js/app.js
+++ files/js/app.js
@@ -1,13 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-      username: 'vuedev',
-      newTweet: '',
-      tweets: [
-        'Vue seems way easier than React so far', 
-        'The syntax is dope tbh', 
-        'Directives!!!! :D'
-      ],
-      bio: 'Front End Web Developer'
+        username: 'vuedev',
+        newTweet: '',
+        tweets: [
+            'Vue seems way easier than React so far',
+            'The syntax is dope tbh',
+            'Directives!!!! :D'
+        ],
+        bio: 'Front End Web Developer'
     }
-  });
+});
